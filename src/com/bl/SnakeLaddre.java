ccbbaab2d4d973c8939ec034aabefb3c5b2935fa
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-07-21 13:33:08 +0530

    Added : find position

--- src/com/bl/SnakeLaddre.java
+++ src/com/bl/SnakeLaddre.java
@@ -2,30 +2,47 @@ package com.bl;
 
 public class SnakeLaddre {
 
-    int position = 0;
+   public static int POSITION = 0;
 
-    public static void diaRoll(){
+     static int diaRoll(){
         int diavalue = (int)(Math.random()*10 % 6)+1;
-        System.out.println(diavalue);
+         System.out.println(diavalue);
+        return diavalue;
     }
 
     public static void play(){
 
-        int number = (int)(Math.random()*10%3);
-        System.out.println(number);
-        switch (number){
-            case 0:
-                System.out.println("Not move the position of plyear");
-                break;
-            case 1:
-                System.out.println("Ladder the player moves ahead");
-                break;
-            case 2:
-                System.out.println("Snake the player moves behind");
-        }
+       while (POSITION < 100){
+
+           int number = (int)(Math.random()*10%3);
+           int diavalue = diaRoll();
+           switch (number){
+               case 0:
+                   System.out.println("Not move the position of plyear");
+                   break;
+               case 1:
+                   System.out.println("Ladder the player moves ahead");
+                   POSITION = POSITION + diavalue;
+                   break;
+               case 2:
+                   System.out.println("Snake the player moves behind");
+                   POSITION = POSITION - diavalue;
+
+               default:{
+                   System.out.println("other option");
+               }
+           }
+           System.out.println("position is :" + " "+ POSITION);
+       }
+    }
+
+    void winplay(){
+
+         int winposition = 100;
     }
     public static void main(String[] args) {
 
+        SnakeLaddre obj = new SnakeLaddre();
         diaRoll();
         play();
 

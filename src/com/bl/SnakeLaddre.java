844417dab9d7ba37b6e21d13f8f688ebeb8049e9
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-07-21 12:28:20 +0530

    Added : diaroll

--- src/com/bl/SnakeLaddre.java
+++ src/com/bl/SnakeLaddre.java
@@ -3,7 +3,14 @@ package com.bl;
 public class SnakeLaddre {
 
     int position = 0;
+
+    public static void diaRoll(){
+        int diavalue = (int)(Math.random()*10 % 6)+1;
+        System.out.println(diavalue);
+    }
     public static void main(String[] args) {
+
+        diaRoll();
 
     }
 }

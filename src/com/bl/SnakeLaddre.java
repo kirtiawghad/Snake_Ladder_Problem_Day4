9032ebee53223dd57627d45d179d10cf0f526f71
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-07-21 12:47:26 +0530

    Added : check option

--- src/com/bl/SnakeLaddre.java
+++ src/com/bl/SnakeLaddre.java
@@ -8,9 +8,26 @@ public class SnakeLaddre {
         int diavalue = (int)(Math.random()*10 % 6)+1;
         System.out.println(diavalue);
     }
+
+    public static void play(){
+
+        int number = (int)(Math.random()*10%3);
+        System.out.println(number);
+        switch (number){
+            case 0:
+                System.out.println("Not move the position of plyear");
+                break;
+            case 1:
+                System.out.println("Ladder the player moves ahead");
+                break;
+            case 2:
+                System.out.println("Snake the player moves behind");
+        }
+    }
     public static void main(String[] args) {
 
         diaRoll();
+        play();
 
     }
 }

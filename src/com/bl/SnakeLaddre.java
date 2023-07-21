0b0bd2398184e5ba1223a4f09572b06920a4a9d0
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-07-21 14:18:19 +0530

    Added : position set

--- src/com/bl/SnakeLaddre.java
+++ src/com/bl/SnakeLaddre.java
@@ -2,17 +2,18 @@ package com.bl;
 
 public class SnakeLaddre {
 
+    public static final int  WIN_POSITION = 100;
    public static int POSITION = 0;
 
      static int diaRoll(){
         int diavalue = (int)(Math.random()*10 % 6)+1;
-         System.out.println(diavalue);
+         System.out.println("DIAVALUE =" + diavalue);
         return diavalue;
     }
 
     public static void play(){
 
-       while (POSITION < 100){
+       while (POSITION < WIN_POSITION){
 
            int number = (int)(Math.random()*10%3);
            int diavalue = diaRoll();
@@ -23,23 +24,28 @@ public class SnakeLaddre {
                case 1:
                    System.out.println("Ladder the player moves ahead");
                    POSITION = POSITION + diavalue;
+                   if (POSITION > WIN_POSITION){
+                       POSITION = POSITION - diavalue;
+                   }
+                   System.out.println(POSITION);
                    break;
                case 2:
                    System.out.println("Snake the player moves behind");
                    POSITION = POSITION - diavalue;
-
+                   if (POSITION < 0){
+                       POSITION = 0;
+                   }
+                   System.out.println(POSITION);
+                   break;
                default:{
-                   System.out.println("other option");
+                   System.out.println("INVALID");
                }
+               break;
            }
-           System.out.println("position is :" + " "+ POSITION);
+
        }
     }
 
-    void winplay(){
-
-         int winposition = 100;
-    }
     public static void main(String[] args) {
 
         SnakeLaddre obj = new SnakeLaddre();
